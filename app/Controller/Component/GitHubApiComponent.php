13891d52833c8928cb195b1139c1cd5de3835163
hrishikesh <hrishikesh@weboniselab.com>
2013-08-03 16:17:56 +0530

    Add GitHubComponent

---
+++ app/Controller/Component/GitHubApiComponent.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by Webonise Lab.
+ * User: Hrishikesh <hrishikesh@weboniselab.com>
+ * Date: 3/8/13 3:59 PM
+ */
+App::import('Vendor', 'vendor/autoload.php');
+class GitHubApiComponent extends Component{
+
+    /**
+     * @var Controller
+     */
+    protected  $_Controller;
+
+    /**
+     * @comment Catch the controller object in protected variable
+     * @param Controller $controller
+     */
+    public function initialize(Controller $controller) {
+        $this->_Controller = $controller;
+    }
+
+    public function getGitHubClient(){
+
+        /**
+         * @var Github\Client
+         */
+        $client = new Github\Client();
+        //$repositories = $client->api('user')->repositories('ornicar');
+
+        return $client;
+
+    }
+
+}
