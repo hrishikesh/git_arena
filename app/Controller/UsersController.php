0a3bdf4d8b49d33a154c47a976016941d4d6d9ce
hrishikesh <hrishikesh@weboniselab.com>
2013-08-03 15:32:33 +0530

    Removed temp

---
+++ app/Controller/UsersController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by Webonise Lab.
+ * User: Hrishikesh <hrishikesh@weboniselab.com>
+ * Date: 3/8/13 3:25 PM
+ */
+class UsersController extends AppController {
+    public function beforeFilter() {
+        parent::beforeFilter();
+    }
+
+    public function login() {
+
+    }
+}
