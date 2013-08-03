ab0898958ac47f9f75d5c1a7ba2fc68460e440e8
Anamika Verma <anamika@weboniselab.com>
2013-08-03 23:21:16 +0530

    conflicts resolved

--- app/Controller/RepositoriesController.php
+++ app/Controller/RepositoriesController.php
@@ -14,7 +14,7 @@ class RepositoriesController extends AppController {
 
     public function beforeFilter() {
         parent::beforeFilter();
-        $this->Auth->allow('index','activity_feeds');
+        $this->Auth->allow('index', 'activity_feeds');
         $this->layout = 'home';
     }
 
@@ -240,15 +240,116 @@ class RepositoriesController extends AppController {
 
     public function activity_feeds($repoName = null) {
 
-        $this->layout     = 'ajax';
+        $this->layout = 'ajax';
 
         //fetch the responses array based on no. of responses selected
         $repoName = $this->params->query['repoName'];
 
+        //
+        //        pr($repoName);
+        //        exit;
 
-        pr($repoName);exit;
 
+        //        if (!empty($repoName)) {
+        //            $commits = $this->GitHubApi
+        //                ->getGitHubClient()
+        //                ->api('repo')
+        //                ->commits()
+        //                ->all('webonise',
+        //                $repoName,
+        //                array(
+        //                     'sha' => 'develop'
+        //
+        //                ));
+        //        }
 
-//        $this->set(compact('selectedResponseNumber'));
+        //                pr($repo['name']);
+        //                exit;
+
+        $commits = Array(
+            '0' => Array(
+                'sha'       => '8da38b6f198804e3e82c513e2f3d5657ea59adb6',
+                'commit'    => Array(
+                    'author'    => Array(
+                        'name'  => 'hrishikesh',
+                        'email' => 'hrishikesh@weboniselab.com',
+                        'date'  => '2013-08-03T10:50:27Z'
+                    ),
+
+                    'committer' => Array(
+                        'name'  => 'hrishikesh',
+                        'email' => 'hrishikesh@weboniselab.com',
+                        'date'  => '2013-08-03T10:50:27Z',
+                    ),
+
+                    'message'   => 'Deleted cache files',
+                ),
+                'committer' => Array(
+                    'login'       => 'hrishikesh',
+                    'id'          => '3132211',
+                    'avatar_url'  => 'https://secure.gravatar.com/avatar/b83c548a8feab3dabe0db5ff1aa9db01?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png',
+                    'gravatar_id' => 'b83c548a8feab3dabe0db5ff1aa9db01',
+                    'url'         => 'https://api.github.com/users/hrishikesh'
+                ),
+            ),
+
+            '1' => Array(
+                'sha'       => '8b667adabcd6a6a2ba244701557562cb66c0d89e',
+                'commit'    => Array(
+                    'author'    => Array(
+                        'name'  => 'hrishikesh',
+                        'email' => 'hrishikesh@weboniselab.com',
+                        'date'  => '2013-08-03T10:49:23Z',
+                    ),
+
+                    'committer' => Array(
+                        'name'  => 'hrishikesh',
+                        'email' => 'hrishikesh@weboniselab.com',
+                        'date'  => '2013-08-03T10:49:23Z',
+                    ),
+
+                    'message'   => 'Deleted cache files',
+                ),
+                'committer' => Array(
+                    'login'       => 'hrishikesh',
+                    'id'          => '3132211',
+                    'avatar_url'  => 'https://secure.gravatar.com/avatar/b83c548a8feab3dabe0db5ff1aa9db01?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png',
+                    'gravatar_id' => 'b83c548a8feab3dabe0db5ff1aa9db01',
+                    'url'         => 'https://api.github.com/users/hrishikesh',
+                ),
+            ),
+
+            '2' => Array(
+                'sha'       => '8a8a6e745634fc72536fc345573963d80606affb',
+                'commit'    => Array(
+                    'author'    => Array(
+                        'name'  => 'hrishikesh',
+                        'email' => 'hrishikesh@weboniselab.com',
+                        'date'  => '2013-08-03T10:48:55Z',
+                    ),
+
+                    'committer' => Array(
+                        'name'  => 'hrishikesh',
+                        'email' => 'hrishikesh@weboniselab.com',
+                        'date'  => '2013-08-03T10:48:55Z',
+                    ),
+
+                    'message'   => 'Merge branch  of github.com:hrishikesh/git_arena',
+                ),
+                'committer' => Array(
+                    'login'       => 'hrishikesh',
+                    'id'          => '3132211',
+                    'avatar_url'  => 'https://secure.gravatar.com/avatar/b83c548a8feab3dabe0db5ff1aa9db01?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png',
+                    'gravatar_id' => 'b83c548a8feab3dabe0db5ff1aa9db01',
+                    'url'         => 'https://api.github.com/users/hrishikesh',
+                ),
+
+            )
+
+        );
+
+        pr($commits);
+
+        $this->set(compact('commits'));
     }
 }
