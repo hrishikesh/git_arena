35e5c97b15d247e0485f510f2a04490e4eb3343e
hrishikesh <hrishikesh@weboniselab.com>
2013-08-03 11:07:56 +0530

    Added Htmls Controller

---
+++ app/Controller/HtmlsController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by Webonise Lab.
+ * User: Hrishikesh <hrishikesh@weboniselab.com>
+ * Date: 3/8/13 10:48 AM
+ */
+class HtmlsController extends Controller{
+
+    public function beforeRender() {
+        parent::beforeRender();
+    }
+
+    public function beforeFilter() {
+        parent::beforeFilter();
+    }
+}
